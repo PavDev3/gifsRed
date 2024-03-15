90e2b09b3f087f400e8bdde8646d6bb336be5eb7
Pavdev3 <pnunfe@gmail.com>
2024-03-15 16:10:32 +0100

    card and list component

---
+++ src/app/gifs/components/gifs-card-list/gifs-card-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'gifs-card-list',
+  templateUrl: './gifs-card-list.component.html',
+})
+export class GifsCardListComponent {}
--- src/app/gifs/components/gifs-list/gifs-list.component.ts
+++ src/app/gifs/components/gifs-list/gifs-list.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { GifsCardListComponent } from '../gifs-card-list/gifs-card-list.component';
 
 @Component({
   selector: 'app-gifs-list',
   standalone: true,
-  imports: [],
   templateUrl: './gifs-list.component.html',
-  styleUrl: './gifs-list.component.css'
+  styleUrl: './gifs-list.component.css',
+  imports: [GifsCardListComponent],
 })
-export class GifsListComponent {
-
-}
+export class GifsListComponent {}
