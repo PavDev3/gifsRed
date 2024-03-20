824ee4dce2097e32412af48914576cecb5795bf7
Pavdev3 <pnunfe@gmail.com>
2024-03-20 18:12:56 +0100

    fetch http

---
+++ environments/environment.ts
@@ -0,0 +1,2 @@
+export const _apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=';
+export const _apikey = 'KZOvk5FgQX2bD1uLB6s8Y0ooX0UXjOan';
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,33 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { _apiUrl, _apikey } from '../../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+  private http = inject(HttpClient);
   private currentTagHistory: string[] = [];
 
-  constructor() {}
-
+  // Array Tags History
   get tagsHistory(): string[] {
     return [...this.currentTagHistory];
   }
 
+  // How organize the tags history
   private organizeTagsHistory(tag: string) {
     tag = tag.toLowerCase();
-
     const index = this.currentTagHistory.findIndex(
       (t) => t.toLowerCase() === tag
     );
     if (index !== -1) {
       this.currentTagHistory.splice(index, 1);
     }
-
     this.currentTagHistory = this.tagsHistory.splice(0, 9);
   }
 
-  public searchTag(tag: string) {
+  constructor() {}
+
+  // Search Tag
+  async searchTag(tag: string) {
     if (tag.length <= 2) return;
     this.organizeTagsHistory(tag);
-
+    this.http
+      .get(`${_apiUrl}${_apikey}&q=${tag}&limit=10`)
+      .subscribe((response) => console.log(response));
     this.currentTagHistory.unshift(tag);
-    console.log(this.currentTagHistory);
   }
 }
