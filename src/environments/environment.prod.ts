034bd00138662f523129d7953631c659eb9fecb0
Pavdev3 <pnunfe@gmail.com>
2024-03-27 13:32:10 +0100

    apikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: process.env['API_KEY'] || 'defaultValue',
+  apiKey: process.env['API_KEY'],
   apiUrl: 'https://api.giphy.com/v1/gifs/search?api_key=',
 };
